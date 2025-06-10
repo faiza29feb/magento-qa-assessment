5189b82abab78c9531526b5963953e2e2a472bb6
faiza29feb <37223630+faiza29feb@users.noreply.github.com>
2025-06-10 00:38:21 -0400

    modifications

--- cypress-automation/cypress/e2e/pages/WishlistPage.js
+++ cypress-automation/cypress/e2e/pages/WishlistPage.js
@@ -1,16 +1,34 @@
 class WishlistPage {
   visitProductAndAddToWishlist(product) {
     cy.visit(product.url)
-    cy.get('a[title="Add to Wish List"]').first().click()
+
+    // Click "Add to Wish List"
+    cy.get('a[title="Add to Wish List"]', { timeout: 10000 }).first().click()
+
+    // Wait for success confirmation
+    cy.get('.message-success', { timeout: 10000 }).should(
+      'contain.text',
+      'has been added to your Wish List'
+    )
   }
 
   goToWishlist() {
     cy.visit('/wishlist/')
+
+    // More reliable check than .wishlist-items-wrapper
+    cy.get('.product-item', { timeout: 10000 }).should('exist')
   }
 
   moveToCartFromWishlist() {
-    cy.get('button[title="Add to Cart"]').first().click()
-  }
+  cy.get('.wishlist-items-wrapper', { timeout: 10000 }).should('exist')
+
+  cy.get('.wishlist-items-wrapper')
+    .find('button[title="Add to Cart"]')
+    .first()
+    .scrollIntoView()
+    .should('be.visible')
+    .click({ force: true })
+}
 }
 
 export default WishlistPage
--- cypress-automation/cypress/e2e/tests/03_wishlistCheckout.cy.js
+++ cypress-automation/cypress/e2e/tests/03_wishlistCheckout.cy.js
@@ -5,7 +5,20 @@ import testData from '../../fixtures/testData.json'
 describe('Add Products to Wishlist and Checkout', () => {
   const wishlistPage = new WishlistPage()
   const cartPage = new CartPage()
+before(() => {
+  cy.visit('/customer/account/login/')
 
+  // Login using test data
+  cy.get('#email', { timeout: 10000 }).should('be.visible').type(testData.user.email)
+  cy.get('#pass').type(testData.user.password, { log: false })
+  cy.get('#send2').click()
+
+  // Wait for redirect to account page
+  cy.url({ timeout: 10000 }).should('include', '/customer/account/')
+
+  // Confirm welcome message has first name
+  cy.get('.greet.welcome', { timeout: 10000 }).should('contain.text', testData.user.firstName)
+})
   it('adds product to wishlist and moves to cart', () => {
     wishlistPage.visitProductAndAddToWishlist(testData.products[2])
     wishlistPage.goToWishlist()
